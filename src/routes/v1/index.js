f7854b59bc695193c93ba47be79488370014a6e9
aya <ayoob.khodadadi@gmail.com>
2022-09-14 12:40:21 +0430

    feat(code): implement send code by path in url

--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -1,13 +1,27 @@
 import { Router } from "express";
+import DomainController from "@controllers/DomainController";
 import Joi from "joi";
-
-import downloadsRouter from "./domains";
+import ReferrerController from "@controllers/ReferrerController";
 
 const router = Router();
 
-// router.use("/install", installRoutes);
-// router.use("/auth", authRoutes);
-router.use(downloadsRouter);
+const domainController = new DomainController();
+const referrerController = new ReferrerController();
+
+router.get(
+  "/checkDomain",
+  domainController.checkDomain.bind(domainController)
+);
+
+router.get(
+  "/setReferrer",
+  referrerController.setReferrer.bind(referrerController)
+);
+
+router.get(
+  "/pukketWord/:code",
+  referrerController.showReferrer.bind(referrerController)
+);
 
 // Health-check Endpoint
 router.get('/health', (_req, res) => { res.send('200') })
